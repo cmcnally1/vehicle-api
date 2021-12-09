e16000fb143cae29660f50895aa0f0ff22cf326d
Ciaran McNally <ciaran.mcnally94@gmail.com>
2021-12-09 19:18:06 +0000

    Implemented update car functionality. Removed some TODOs for implemented functionality. Regression passing

--- vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/api/CarController.java
@@ -86,13 +86,13 @@ class CarController {
      */
     @PutMapping("/{id}")
     ResponseEntity<?> put(@PathVariable Long id, @Valid @RequestBody Car car) {
-        /**
-         * TODO: Set the id of the input car object to the `id` input.
-         * TODO: Save the car using the `save` method from the Car service
-         * TODO: Use the `assembler` on that updated car and return as part of the response.
-         *   Update the first line as part of the above implementing.
-         */
-        Resource<Car> resource = assembler.toResource(new Car());
+
+        // Set the input ID to be the ID of the input car
+        car.setId(id);
+        // Update the given car using save
+        Car savedCar = carService.save(car);
+        // Return the saved car to indicate that to the user that the car is saved.
+        Resource<Car> resource = assembler.toResource(savedCar);
         return ResponseEntity.ok(resource);
     }
 
@@ -103,9 +103,6 @@ class CarController {
      */
     @DeleteMapping("/{id}")
     ResponseEntity<?> delete(@PathVariable Long id) {
-        /**
-         * TODO: Use the Car Service to delete the requested vehicle.
-         */
         // Use the car service to delete the car with given id
         carService.delete(id);
         return ResponseEntity.noContent().build();
